4cca5c04ac31de3e9923191a4566d2cbedec9373
hasan <121497324+HOMEALONE2003@users.noreply.github.com>
2026-02-04 15:35:50 +0330

    Add files via upload

---
+++ first.py
@@ -0,0 +1,14 @@
+#function e.g
+username=hasan
+build_version=classified
+ip_add=192.168.1.1
+massage= f"{username} is using {build_version} with {ip_add}"
+print(massage)
+
+#No input function e.g
+
+input_name=input("enter your name here")
+    print(f 'hello {input_name}!')
+    
+    
+
